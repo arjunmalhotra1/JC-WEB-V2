c342bcb391ded454f903e5437772cfa0f037d5b1
Arjun Malhotra <am2cj@virginia.edu>
2022-01-25 20:48:09 -0600

    2.Creating-a-contact-Page

--- Section-1-Getting-Started/1.A-Basic-Web-Application/main.go
+++ Section-1-Getting-Started/1.A-Basic-Web-Application/main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
-func HandlerFunc(w http.ResponseWriter, req *http.Request) {
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	//w.Header().Set("Content-type", "text/plain")
+	w.Header().Set("Content-type", "text/html;charset=utf-8")
 	fmt.Fprintf(w, "<h1> Welcome to my awesome site </h1>")
 }
 
+func contactHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/html;charset=utf-8")
+	fmt.Fprintf(w, "<h1> Contact Page </h1> <p>To get in touch, email me at <a href=\"mailto:arjun1malhotra@gmail.com\">arjun1malhotra@gmail.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", HandlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on :3000....")
 	http.ListenAndServe(":3000", nil)
 }
