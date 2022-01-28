910fbf899863e92eca91c4343f5ceeb7bc47855d
Arjun Malhotra <am2cj@virginia.edu>
2022-01-28 06:38:17 -0600

    URL-Path vs Raw Path

--- Section-1-Getting-Started/1.A-Basic-Web-Application/main.go
+++ Section-1-Getting-Started/1.A-Basic-Web-Application/main.go
@@ -16,9 +16,28 @@ func contactHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<h1> Contact Page </h1> <p>To get in touch, email me at <a href=\"mailto:arjun1malhotra@gmail.com\">arjun1malhotra@gmail.com</a>.")
 }
 
+func pathHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, req.URL.Path)
+	fmt.Fprintln(w, req.URL.RawPath)
+
+	// switch req.URL.Path {
+	// case "/":
+	// 	homeHandler(w, req)
+	// case "/contact":
+	// 	contactHandler(w, req)
+
+	// default:
+	// 	//TODO: handle page not found error
+	// 	http.Error(w, "Page not found", http.StatusNotFound)
+
+	// }
+
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on :3000....")
 	http.ListenAndServe(":3000", nil)
 }
